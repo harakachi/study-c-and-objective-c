4c6b3e924de8c2db6b01ddd37e2697c474c8ae73
harakachi <harakachi@gmail.com>
2013-06-23 18:17:14 +0900

    Add AGoodStart

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by 原田 勝信 on 2013/06/23.
+//  Copyright (c) 2013年 原田 勝信. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    return 0;
+}
+
