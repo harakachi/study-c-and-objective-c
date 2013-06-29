d000f6c78d976f31954b33b255d34e1b056018eb
harakachi <harakachi@gmail.com>
2013-06-30 07:55:39 +0900

    Add section 13, 14

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -12,7 +12,7 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        NSDate *now = [NSDate date];
+        NSDate *now = [[NSDate alloc] init];
         NSLog(@"The new date lives at %p", now);
         NSLog(@"The date is %@", now);
         
@@ -22,16 +22,31 @@ int main(int argc, const char * argv[])
         NSDate *later = [now dateByAddingTimeInterval:100000];
         NSLog(@"In 100,000 seconds it will be %@", later);
         
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSUInteger day = [cal ordinalityOfUnit:NSDayCalendarUnit
+                                        inUnit:NSMonthCalendarUnit
+                                       forDate:now];
+        NSLog(@"This is day %lu of the month", day);
+        
+        // Challenge
         NSDateComponents *comps = [[NSDateComponents alloc] init];
         [comps setYear:1980];
         [comps setMonth:9];
         [comps setDay:18];
         
-        // Challenge
         NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
         NSDate *dateOfBirth = [g dateFromComponents:comps];
         double d = [now timeIntervalSinceDate:dateOfBirth];
         NSLog(@"interval = %f", d);
+        
+        // challenge section 13
+        NSTimeZone *tz = [NSTimeZone systemTimeZone];
+        NSLog(@"this timezone a summertime %@\n", [tz isDaylightSavingTime] ? @"YES" : @"NO");
+        
+        // challenge section 14
+        NSHost *host = [NSHost currentHost];
+        NSString *localname = [host localizedName];
+        NSLog(@"This computer localname is %@\n", localname);
     }
     return 0;
 }
