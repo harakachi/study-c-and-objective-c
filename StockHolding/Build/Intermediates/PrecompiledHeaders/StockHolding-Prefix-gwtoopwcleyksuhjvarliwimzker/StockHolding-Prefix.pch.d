1a31c0fd79b6cefe179b9df1307ee437c8efdfcf
harakachi <harakachi@gmail.com>
2013-06-30 15:17:53 +0900

    Add all files.

---
+++ StockHolding/Build/Intermediates/PrecompiledHeaders/StockHolding-Prefix-gwtoopwcleyksuhjvarliwimzker/StockHolding-Prefix.pch.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/harada/Desktop/work/study-c-and-objective-c/StockHolding/StockHolding/StockHolding-Prefix.pch
---
+++ StockHolding/Build/Intermediates/StockHolding.build/Debug/StockHolding.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/harada/Desktop/work/study-c-and-objective-c/StockHolding/StockHolding/main.m \
+  /Users/harada/Desktop/work/study-c-and-objective-c/StockHolding/StockHolding.h
