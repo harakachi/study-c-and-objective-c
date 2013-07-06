18e5247073ecabb5cbfcd84eb79d377585cff716
harakachi <harakachi@gmail.com>
2013-07-06 16:00:14 +0900

    Add section 21

--- BMITime/BMITime/main.m
+++ BMITime/BMITime/main.m
@@ -15,12 +15,22 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         NSMutableArray *employees = [[NSMutableArray alloc] init];
+        
+        NSMutableDictionary *executives = [[NSMutableDictionary alloc] init];
+        
         for (int i=0; i < 10; i++) {
             Employee *person = [[Employee alloc] init];
             [person setWeightInKilos:58+i];
             [person setHeightInMeters:1.6*i];
             [person setEmployeeID:i];
             [employees addObject:person];
+            
+            if (i == 0) {
+                [executives setObject:person forKey:@"CEO"];
+            }
+            if (i == 1) {
+                [executives setObject:person forKey:@"CTO"];
+            }
         }
         
         NSMutableArray *allAssets = [[NSMutableArray alloc] init];
@@ -38,12 +48,29 @@ int main(int argc, const char * argv[])
             [allAssets addObject:assets];
         }
         
+        NSSortDescriptor *voa =
+            [NSSortDescriptor sortDescriptorWithKey:@"valueOfAssets"
+                                          ascending:YES];
+        NSSortDescriptor *ei =
+            [NSSortDescriptor sortDescriptorWithKey:@"employeeID"
+                                          ascending:YES];
+        [employees sortUsingDescriptors:[NSArray arrayWithObjects:voa, ei, nil]];
+        
         NSLog(@"Employees: %@", employees);
         NSLog(@"Giving up ownership of one employee");
         [employees removeObjectAtIndex:5];
-        
         NSLog(@"allAssets: %@", allAssets);
         
+        NSLog(@"executives: %@", executives);
+        executives = nil;
+        
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"holder.valueOfAssets > 70"];
+        NSArray *toBeclaimed = [allAssets filteredArrayUsingPredicate:predicate];
+        NSLog(@"toBeReclaimed: %@", toBeclaimed);
+        
+        
+        
+        toBeclaimed = nil;
         allAssets = nil;
         employees = nil;
         //float bmi = [person bodyMassindex];
